b6aa5d1a3682b1f5a6912523cbea616165234fa1
nikant12 <nikant12@users.sourceforge.net>
2013-04-14 09:17:07 +0000

    small changes to hi-en dictionary

--- dev/inconsistency.sh
+++ dev/inconsistency.sh
@@ -7,8 +7,8 @@ rm ../tmp1gen.xsl
 lt-expand ../apertium-en-hi.en.dixtmp1 | grep -v '<prn><enc>' | grep -e ':<:' -e '\w:\w' | sed 's/:<:/%/g' | sed 's/:/%/g' | cut -f2 -d'%' |  sed 's/^/^/g' | sed 's/$/$ ^.<sent>$/g' | tee $TMPDIR/tmp_testvoc1.txt |
         apertium-pretransfer|
         apertium-transfer ../apertium-en-hi.en-hi.t1x  ../en-hi.t1x.bin  ../en-hi.autobil.bin |
-        apertium-interchunk ../apertium-en-hi.en-hi.t2x  ../en-hi.t2x.bin |
-        apertium-postchunk ../apertium-en-hi.en-hi.t3x  ../en-hi.t3x.bin  | tee $TMPDIR/tmp_testvoc2.txt |
+         apertium-interchunk ../apertium-en-hi.en-hi.t2x  ../en-hi.t2x.bin |
+        apertium-postchunk ../apertium-en-hi.en-hi.t3x  ../en-hi.t3x.bin  |tee $TMPDIR/tmp_testvoc2.txt |
         lt-proc -d ../en-hi.autogen.bin > $TMPDIR/tmp_testvoc3.txt
 paste -d _ $TMPDIR/tmp_testvoc1.txt $TMPDIR/tmp_testvoc2.txt $TMPDIR/tmp_testvoc3.txt | sed 's/\^.<sent>\$//g' | sed 's/_/   --------->  /g'
 
