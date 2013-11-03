567c62096fcfcac987328b6f705d82903e072c16
Francis M. Tyers <ftyers@prompsit.com>
2013-11-03 15:02:32 +0000

    update testvoq

--- dev/testvoc/inconsistency.sh
+++ dev/testvoc/inconsistency.sh
@@ -4,24 +4,25 @@ DIR=$1
 
 if [[ $DIR = "hin-eng" ]]; then
 
-lt-expand ../../apertium-en-hi.hi.dix | grep -v '<prn><enc>' | grep -v 'REGEX' | grep -v ':<:' | sed 's/:>:/%/g' | sed 's/:/%/g' | cut -f2 -d'%' |  sed 's/^/^/g' | sed 's/$/$ ^.<sent>$/g' | tee $TMPDIR/$DIR.tmp_testvoc1.txt |
+lt-expand ../../apertium-eng-hin.hin.dix | grep -v '<prn><enc>' | grep -v 'REGEX' | grep -v ':<:' | sed 's/:>:/%/g' | sed 's/:/%/g' | cut -f2 -d'%' |  sed 's/^/^/g' | sed 's/$/$ ^.<sent>$/g' | tee $TMPDIR/$DIR.tmp_testvoc1.txt |
         apertium-pretransfer|
-	lt-proc -b ../../hi-en.autobil.bin |  tee $TMPDIR/$DIR.tmp_testvoc2.txt |
-        apertium-transfer -b ../../apertium-en-hi.hi-en.t1x  ../../hi-en.t1x.bin |
-        apertium-interchunk ../../apertium-en-hi.hi-en.t2x  ../../hi-en.t2x.bin |
-        apertium-postchunk ../../apertium-en-hi.hi-en.t3x  ../../hi-en.t3x.bin  | tee $TMPDIR/$DIR.tmp_testvoc3.txt |
-        lt-proc -d ../../hi-en.autogen.bin > $TMPDIR/$DIR.tmp_testvoc4.txt
+	lt-proc -b ../../hin-eng.autobil.bin |  tee $TMPDIR/$DIR.tmp_testvoc2.txt |
+        apertium-transfer -b ../../apertium-eng-hin.hin-eng.t1x  ../../hin-eng.t1x.bin |
+        apertium-interchunk ../../apertium-eng-hin.hin-eng.t2x  ../../hin-eng.t2x.bin |
+        apertium-interchunk ../../apertium-eng-hin.hin-eng.t3x  ../../hin-eng.t3x.bin |
+        apertium-postchunk ../../apertium-eng-hin.hin-eng.t4x  ../../hin-eng.t4x.bin  | tee $TMPDIR/$DIR.tmp_testvoc3.txt |
+        lt-proc -d ../../hin-eng.autogen.bin > $TMPDIR/$DIR.tmp_testvoc4.txt
 paste -d _ $TMPDIR/$DIR.tmp_testvoc1.txt $TMPDIR/$DIR.tmp_testvoc2.txt $TMPDIR/$DIR.tmp_testvoc3.txt $TMPDIR/$DIR.tmp_testvoc4.txt | sed 's/\^.<sent>\$//g' | sed 's/_/   --------->  /g'
 
 elif [[ $DIR = "eng-hin" ]]; then
 
-lt-expand ../../apertium-en-hi.en.dix | grep -v '<prn><enc>' | grep -v 'REGEX' | grep -v ':<:' | sed 's/:>:/%/g' | sed 's/:/%/g' | cut -f2 -d'%' |  sed 's/^/^/g' | sed 's/$/$ ^.<sent>$/g' | tee $TMPDIR/$DIR.tmp_testvoc1.txt |
+lt-expand ../../apertium-eng-hin.eng.dix | grep -v '<prn><enc>' | grep -v 'REGEX' | grep -v ':<:' | sed 's/:>:/%/g' | sed 's/:/%/g' | cut -f2 -d'%' |  sed 's/^/^/g' | sed 's/$/$ ^.<sent>$/g' | tee $TMPDIR/$DIR.tmp_testvoc1.txt |
         apertium-pretransfer|
-	lt-proc -b ../../en-hi.autobil.bin | tee $TMPDIR/$DIR.tmp_testvoc2.txt |
-        apertium-transfer -b ../../apertium-en-hi.en-hi.t1x  ../../en-hi.t1x.bin |
-        apertium-interchunk ../../apertium-en-hi.en-hi.t2x  ../../en-hi.t2x.bin |
-        apertium-postchunk ../../apertium-en-hi.en-hi.t3x  ../../en-hi.t3x.bin  | tee $TMPDIR/$DIR.tmp_testvoc3.txt |
-        lt-proc -d ../../en-hi.autogen.bin > $TMPDIR/$DIR.tmp_testvoc4.txt
+	lt-proc -b ../../eng-hin.autobil.bin | tee $TMPDIR/$DIR.tmp_testvoc2.txt |
+        apertium-transfer -b ../../apertium-eng-hin.eng-hin.t1x  ../../eng-hin.t1x.bin |
+        apertium-interchunk ../../apertium-eng-hin.eng-hin.t2x  ../../eng-hin.t2x.bin |
+        apertium-postchunk ../../apertium-eng-hin.eng-hin.t3x  ../../eng-hin.t3x.bin  | tee $TMPDIR/$DIR.tmp_testvoc3.txt |
+        lt-proc -d ../../eng-hin.autogen.bin > $TMPDIR/$DIR.tmp_testvoc4.txt
 paste -d _ $TMPDIR/$DIR.tmp_testvoc1.txt $TMPDIR/$DIR.tmp_testvoc2.txt $TMPDIR/$DIR.tmp_testvoc3.txt $TMPDIR/$DIR.tmp_testvoc4.txt| sed 's/\^.<sent>\$//g' | sed 's/_/   --------->  /g'
 
 
