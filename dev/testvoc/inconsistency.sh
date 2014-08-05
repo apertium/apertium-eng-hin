4f11de97838ec125835c52abf2220a6add553425
Francis M. Tyers <ftyers@prompsit.com>
2014-08-05 11:51:54 +0000

    update testvoc

--- dev/testvoc/inconsistency.sh
+++ dev/testvoc/inconsistency.sh
@@ -1,30 +1,24 @@
+#!/usr/bin/env bash
+
 TMPDIR=/tmp
 
-DIR=$1
+DICT=$1
+DIR=$2
+
+SED=sed
 
 if [[ $DIR = "hin-eng" ]]; then
 
-lt-expand ../../apertium-eng-hin.hin.dix | grep -v '<prn><enc>' | grep -v 'REGEX' | grep -v ':<:' | sed 's/:>:/%/g' | sed 's/:/%/g' | cut -f2 -d'%' |  sed 's/^/^/g' | sed 's/$/$ ^.<sent>$/g' | tee $TMPDIR/$DIR.tmp_testvoc1.txt |
-        apertium-pretransfer|
-	lt-proc -b ../../hin-eng.autobil.bin |  tee $TMPDIR/$DIR.tmp_testvoc2.txt |
-        apertium-transfer -b ../../apertium-eng-hin.hin-eng.t1x  ../../hin-eng.t1x.bin |
-        apertium-interchunk ../../apertium-eng-hin.hin-eng.t2x  ../../hin-eng.t2x.bin |
-        apertium-interchunk ../../apertium-eng-hin.hin-eng.t3x  ../../hin-eng.t3x.bin |
-        apertium-postchunk ../../apertium-eng-hin.hin-eng.t4x  ../../hin-eng.t4x.bin  | tee $TMPDIR/$DIR.tmp_testvoc3.txt |
+# Run the bilingual dictionary before to make sure we are only checking things we have.
+lt-expand $DICT | grep -v '<prn><enc>' | grep -v 'REGEX' | grep -v ':<:' | $SED 's/:>:/%/g' | $SED 's/:/%/g' | cut -f2 -d'%' |  $SED 's/^/^/g' | $SED 's/$/$ ^.<sent>$/g' | apertium-pretransfer | lt-proc -b ../../hin-eng.autobil.bin | grep -v '/@' | cut -f1 -d'/' | $SED 's/$/$ ^.<sent>$/g' | tee $TMPDIR/$DIR.tmp_testvoc1.txt |\
+        apertium-pretransfer|\
+	lt-proc -b ../../hin-eng.autobil.bin | tee $TMPDIR/$DIR.tmp_testvoc2.txt |\
+        apertium-transfer -b ../../apertium-eng-hin.hin-eng.t1x  ../../hin-eng.t1x.bin |\
+	apertium-interchunk ../../apertium-eng-hin.hin-eng.t2x ../../hin-eng.t2x.bin |\
+	apertium-interchunk ../../apertium-eng-hin.hin-eng.t3x ../../hin-eng.t3x.bin |\
+	apertium-postchunk ../../apertium-eng-hin.hin-eng.t4x ../../hin-eng.t4x.bin | tee $TMPDIR/$DIR.tmp_testvoc3.txt |\
         lt-proc -d ../../hin-eng.autogen.bin > $TMPDIR/$DIR.tmp_testvoc4.txt
-paste -d _ $TMPDIR/$DIR.tmp_testvoc1.txt $TMPDIR/$DIR.tmp_testvoc2.txt $TMPDIR/$DIR.tmp_testvoc3.txt $TMPDIR/$DIR.tmp_testvoc4.txt | sed 's/\^.<sent>\$//g' | sed 's/_/   --------->  /g'
-
-elif [[ $DIR = "eng-hin" ]]; then
-
-lt-expand ../../apertium-eng-hin.eng.dix | grep -v '<prn><enc>' | grep -v 'REGEX' | grep -v ':<:' | sed 's/:>:/%/g' | sed 's/:/%/g' | cut -f2 -d'%' |  sed 's/^/^/g' | sed 's/$/$ ^.<sent>$/g' | tee $TMPDIR/$DIR.tmp_testvoc1.txt |
-        apertium-pretransfer|
-	lt-proc -b ../../eng-hin.autobil.bin | tee $TMPDIR/$DIR.tmp_testvoc2.txt |
-        apertium-transfer -b ../../apertium-eng-hin.eng-hin.t1x  ../../eng-hin.t1x.bin |
-        apertium-interchunk ../../apertium-eng-hin.eng-hin.t2x  ../../eng-hin.t2x.bin |
-        apertium-postchunk ../../apertium-eng-hin.eng-hin.t3x  ../../eng-hin.t3x.bin  | tee $TMPDIR/$DIR.tmp_testvoc3.txt |
-        lt-proc -d ../../eng-hin.autogen.bin > $TMPDIR/$DIR.tmp_testvoc4.txt
-paste -d _ $TMPDIR/$DIR.tmp_testvoc1.txt $TMPDIR/$DIR.tmp_testvoc2.txt $TMPDIR/$DIR.tmp_testvoc3.txt $TMPDIR/$DIR.tmp_testvoc4.txt| sed 's/\^.<sent>\$//g' | sed 's/_/   --------->  /g'
-
+paste -d _ $TMPDIR/$DIR.tmp_testvoc1.txt $TMPDIR/$DIR.tmp_testvoc2.txt $TMPDIR/$DIR.tmp_testvoc3.txt $TMPDIR/$DIR.tmp_testvoc4.txt | $SED 's/\^.<sent>\$//g' | $SED 's/_/   --------->  /g'
 
 else
 	echo "bash inconsistency.sh <direction>";
--- dev/testvoc/testvoc.sh
+++ dev/testvoc/testvoc.sh
@@ -1,10 +1,11 @@
-if [[ $1 = "hin-eng" ]]; then
+#!/usr/bin/env bash
+
+if [[ $2 = "hin-eng" ]]; then
 echo "==Hindi->English===========================";
-bash inconsistency.sh hin-eng > /tmp/hin-eng.testvoc; bash inconsistency-summary.sh /tmp/hin-eng.testvoc hin-eng
+bash inconsistency.sh $1 hin-eng > /tmp/hin-eng.testvoc; bash inconsistency-summary.sh /tmp/hin-eng.testvoc hin-eng
 echo ""
-
-elif [[ $1 = "eng-hin" ]]; then
+elif [[ $2 = "eng-hin" ]]; then
 echo "==English->Hindi===========================";
-bash inconsistency.sh eng-hin > /tmp/eng-hin.testvoc; bash inconsistency-summary.sh /tmp/eng-hin.testvoc eng-hin
-
+bash inconsistency.sh $1 eng-hin > /tmp/eng-hin.testvoc; bash inconsistency-summary.sh /tmp/eng-hin.testvoc eng-hin
+echo "";
 fi
